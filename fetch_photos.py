31b9007eac050467fd9b9bc119fbc81ae0691456
Marko Golovko <marko.golovko@coraltravel.pl>
2024-10-21 09:48:08 +0200

    add fetch photos

--- fetch_photos.py
+++ fetch_photos.py
@@ -9,7 +9,7 @@ load_dotenv()
 client = pymongo.MongoClient(os.environ["MONGO_HOST"])
 db = client["hotels"]
 hotel_col = db['hotels']
-col = db["reviews"]
+col = db["photos"]
 
 existing_ids = set([str(i['_id'])
                    for i in col.aggregate([{"$group": {"_id": "$location_id"}}])])
@@ -21,16 +21,18 @@ total = len(ids_set)
 proceed = 0
 for hotel_id in ids_set:
   if hotel_id not in existing_ids:
-    url = f'https://api.content.tripadvisor.com/api/v1/location/{hotel_id}/reviews?key={os.environ["TRIPADVISOR_KEY"]}&language=en'
+    url = f'https://api.content.tripadvisor.com/api/v1/location/{hotel_id}/photos?key={os.environ["TRIPADVISOR_KEY"]}&language=en&source=Expert'
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers, timeout=500).text
     response_json = json.loads(response)
     if 'message' in response_json:
       print(response_json['message'])
+      if 'message' == 'Limit Exceeded':
+        break
     else:
-      for review in response_json['data']:
-        filter_criteria = {'id': review['id']}
-        col.update_one(filter_criteria, {'$set': review}, upsert=True)
+      filter_criteria = {'location_id': hotel_id}
+      photos = {'photos': response_json['data'], 'location_id': hotel_id}
+      col.update_one(filter_criteria, {'$set': photos}, upsert=True)
       added_ids.add(hotel_id)
       proceed += 1
       print(f'{proceed}/{total}')
